87a7e2753cc1a0ddafb4cae684d513a07da0efc4
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-12-20 15:01:55 +0800

    chore: init project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: use_super_parameters
+
+import 'package:flutter/material.dart';
+import 'package:wisata_bandung/detail_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
